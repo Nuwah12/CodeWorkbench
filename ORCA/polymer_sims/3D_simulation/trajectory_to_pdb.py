f90e31e97320e5fd1a8bb892172497e0da842b0a
Nuwah12 <ngb23@pitt.edu>
2024-03-25 10:28:26 -0400

    sim outs to pdb

---
+++ ORCA/polymer_sims/3D_simulation/trajectory_to_pdb.py
@@ -0,0 +1,16 @@
+##########
+# Script for transforming simulation outs to PDB files
+# First fetches coordinates from ONE h5 trajectory file, and then converts to .pdb format
+# Directory in `data` must contain one `.h5` file with `n` blocks 
+##########
+
+import polymerutils
+
+data = "/mnt/data0/noah/analysis/misc-analysis/ORCA/polymer_sims/3D_simulation/sim_outs/"
+n = 100
+
+# Fetch blocks
+for i in range(m):
+    t = polymerutils.fetch_block(data, i)
+    # Write as pdb
+    polymerutils.save(t, filename="blocks_0-99_conf{}.pdb".format(i), mode="pdb")
