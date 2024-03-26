4e687da47ade77372ddb5230cb441639c7cb1a65
Nuwah12 <ngb23@pitt.edu>
2024-03-26 13:10:02 -0400

    save h5 outs to txt

---
+++ ORCA/polymer_sims/3D_simulation/trajectory_to_txt.py
@@ -0,0 +1,17 @@
+###############
+# Using polychrom polymerutils module to transform h5 --> text x,y,z
+# Noah Burget
+# 3/26/24
+###############
+import os
+import sys
+import polychrom.polymerutils as polu
+
+if len(sys.argv) != 2:
+    print('Usage: python3 trajectory_to_txt.py <path to h5>')
+    os._exit(1)
+n = 100
+for i in range(n):
+    t = polu.fetch_block(sys.argv[1], i)
+    # Write as pdb
+    polu.save(t, filename="blocks_0-99_conf{}.txt".format(i), mode="txt")
