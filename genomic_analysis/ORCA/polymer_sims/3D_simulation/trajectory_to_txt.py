2c7a6eb8e1c255f32490ef7c8afecad1ed42026b
Nuwah12 <nburget1@hotmail.com>
2024-05-08 14:48:53 -0400

    greater ease of use

--- genomic_analysis/ORCA/polymer_sims/3D_simulation/trajectory_to_txt.py
+++ genomic_analysis/ORCA/polymer_sims/3D_simulation/trajectory_to_txt.py
@@ -1,3 +1,4 @@
+#!/home/noah/.conda/envs/noah/bin/python3
 ###############
 # Using polychrom polymerutils module to transform h5 --> text x,y,z
 # Noah Burget
@@ -6,12 +7,20 @@
 import os
 import sys
 import polychrom.polymerutils as polu
-
-if len(sys.argv) != 2:
-    print('Usage: python3 trajectory_to_txt.py <path to h5>')
+from pathlib import Path
+if len(sys.argv) != 3:
+    print('Usage: python3 trajectory_to_txt.py <total number of confs> <path to h5>')
     os._exit(1)
-n = 1000
+
+d = "confs_txt"
+p = Path(d)
+if not p.exists():
+    p.mkdir()
+else:
+    print('Delete ./confs_txt and re-run.')
+
+n=int(sys.argv[1])
 for i in range(n):
-    t = polu.fetch_block(sys.argv[1], i)
-    # Write as pdb
-    polu.save(t, filename="blocks_0-99_conf{}.txt".format(i), mode="txt")
+    t = polu.fetch_block(sys.argv[2], i)
+    # Write as text file
+    polu.save(t, filename="{}/conf{}.txt".format(d, i), mode="txt")
