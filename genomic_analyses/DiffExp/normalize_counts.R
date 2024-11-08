579b9fbc215c612f77bc2f996916b8305e917e2e
Nuwah12 <nburget1@hotmail.com>
2024-11-08 13:16:04 -0500

    RPKM/RPK norm

---
+++ genomic_analyses/DiffExp/normalize_counts.R
@@ -0,0 +1,25 @@
+####################
+# Functions for common normalization techniques of genomic count data (RPM, RPKM)
+# Noah Burget
+# 7/16/24
+####################
+
+rpkm <- function(x){
+  if(ncol(x) < 3){
+    stop('Table must contain three columns in this order: Gene ID, Gene length in bp, and raw counts.')
+  }
+  totalReads <- sum(x[,3])
+  factor <- (x[,2]/1E3)*(totalReads/1E6)
+  x$rpkm <- x[,3] / factor
+  return(x)
+}
+
+rpm <- function(x){
+  if(ncol(x) < 2){
+    stop('Table must contain two columns in this order: Gene ID, and raw counts.')
+  }
+  totalReads <- sum(x[,2])
+  factor <- totalReads/1E6
+  x$rpm <- x[,2] / factor
+  return(m)  
+}
