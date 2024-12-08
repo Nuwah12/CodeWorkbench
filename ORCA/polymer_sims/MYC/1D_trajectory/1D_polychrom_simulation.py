6631313cdd4c7ee772279a432e310e6901ff5014
Nuwah12 <nburget1@hotmail.com>
2024-12-07 22:13:27 -0500

    better documentation for 1D driver script

--- ORCA/polymer_sims/MYC/1D_trajectory/1D_polychrom_simulation.py
+++ ORCA/polymer_sims/MYC/1D_trajectory/1D_polychrom_simulation.py
@@ -1,5 +1,6 @@
 ###################
 # Noah Burget
+# Last updated: 12/7/24
 # Driver script for the 1-Dimensional Portion of the loop extrusion simulations
 # Originally written as a jupyter notebook, but converted to .py script for simplicity
 ###################
@@ -14,9 +15,9 @@ import h5py
 import sys
 
 ### Translocating and writing trajectories using custom extruder class
-RUN_NAME = "MYC_Granta519_onlyEBF1Blocking_randomLoading_112524" #### Define a name for this simulation run, sowe can save parameters
+RUN_NAME = "MYC_Granta519_perturb_EBF1Blocking_randomLoading_tieredBlockingBasedOnEBF1Strength" #### Define a name for this simulation run, sowe can save parameters
 N1 = 900 # Size of 1 system
-front_buffer = 0
+front_buffer = 0 # 'buffer' zone for poltting purposes
 end_buffer = 0
 N1 = N1 + front_buffer + end_buffer
 M = 1 # No. of systems
@@ -25,8 +26,9 @@ occupied = np.zeros(N) # List to tell if current monomer is occupied by an extru
 occupied[0] = 1 
 occupied[-1] = 1
 steps = 50000 # Timesteps for 1D sim.
-LEFNum = 20 # No. of RANDOMLY LOADED extruders
 num_chunks = 50 # No. of chunks to write trajectories in
+LIFETIME = 800 # Cohesin lifetime
+LIFETIME_STALLED = LIFETIME // 10 # Cohesin lifetime when stalled
 
 ### Blockers (i.e. CTCF) - {pos. : prob.}
 left_blockers_capture = {}
@@ -38,49 +40,65 @@ TIER1_BLOCK = [0.99, 0.0001]
 TIER2_BLOCK = [0.75, 0.01]
 TIER3_BLOCK = [0.5, 0.015] ### Increasing probability of release
 TIERB3_BLOCK = [0.4, 0.02]
-TIER4_BLOCK = [0.25, 0.03]
+TIER4_BLOCK = [0.1, 0.1]
 
+#####################################################################
 ### Defining 'blocking regions' - consecutive monomers that can block
-E1_blockingRegion_1 = np.arange(181+front_buffer, 185+front_buffer)
-E1_blockingRegion_2 = np.arange(210+front_buffer, 220+front_buffer)
-E2_blockingRegion = np.arange(304+front_buffer, 307+front_buffer)
-B3_blockingRegion_forward = np.arange(568+front_buffer, 570+front_buffer)
-B3_blockingRegion_reverse = np.arange(576+front_buffer, 578+front_buffer)
-MYC_blockingRegion = np.arange(737+front_buffer, 742+front_buffer)
-blockingRegions = {'E1_1':E1_blockingRegion_1, 'E1_2_EBF1':E1_blockingRegion_2, 'E2_EBF1':E2_blockingRegion, 'B3+_EBF1':B3_blockingRegion_forward, 'B3-':B3_blockingRegion_reverse, 'MYC':MYC_blockingRegion}
+# Each call below defines the blocking region as (low, high), EXCLUSIVE on the upper bound
+#####################################################################
+E1_blockingRegion_1 = np.arange(181+front_buffer, 185+front_buffer) # Boundary, not EBF1 involved
+E1_blockingRegion_2 = np.arange(210+front_buffer, 220+front_buffer) # E1, EBF1 involved (50, STRONG)
+E2_blockingRegion = np.arange(304+front_buffer, 312+front_buffer) # E2, EBF1 involved (15, MED)
+#B3_blockingRegion_forward = np.arange(553+front_buffer, 570+front_buffer) # B3+, EBF1 involved (8, WEAK)
+B3_blockingRegion_reverse = np.arange(576+front_buffer, 578+front_buffer) # B3-, not EBF1 involved
+MYC_blockingRegion = np.arange(737+front_buffer, 742+front_buffer) # MYC, not EBF1 involved
+
+###########################################################################
+### Any EBF1-associated blockers below should have '_EBF1' as the suffix in the keys of the dictionary below
+###########################################################################
+blockingRegions = {'E1_1':E1_blockingRegion_1, 'E1_2':E1_blockingRegion_2, 'E2':E2_blockingRegion, 'B3-': B3_blockingRegion_reverse, 'MYC':MYC_blockingRegion}
+
+### Assign each blocking region its capture and release probabilities (0<=x<=1)
 for br in blockingRegions:
-    if br == 'E1_1' or 'E1_2' in br:
-        cap = 0.6
-        rel = 0.015
-    elif 'E2' in br or 'B3+' in br:
+    # Boundary blocker
+    if br == 'E1_1': # Will never be EBF1 involved
+        cap = 0.99
+        rel = 0.001
+    # E1 blocker
+    if 'E1_2' in br: # EBF1 involved
         cap = 0.45
-        rel = 0.03
-    elif br == 'B3-':
-        cap = 0.45
-        rep = 0.03
-    elif br == 'MYC':
-        cap = 0.6
-        rel = 0.015
-    if 'EBF1' in br: # IF EBF1 blocker, bidirectional
+        rel = 0.05
+    # E2 blocker
+    elif 'E2' in br: # EBF1 involved
+        cap = 0.25
+        rel = 0.15
+    # B3 blocker
+    elif 'B3' in br: # EBF1 involved
+        cap = 0.3
+        rel = 0.01
+    # MYC promoter blocker
+    elif br == 'MYC': # Unchanged
+        cap = 0.75
+        rel = 0.01
+    # Now, also according to the dictionary key, we assign the directional blockers with the previously defined probabilities
+    if 'EBF1' in br: # IF EBF1 blocker, bidirectional (in the EBF1 loading system, and the perturbed EBF1 blocking system, this will never happen)
         for loc in blockingRegions[br]:
             left_blockers_capture[loc] = cap
             left_blockers_release[loc] = rel
             right_blockers_capture[loc] = cap
             right_blockers_release[loc] = rel
-    elif br == 'MYC':
+    # The below two conditions will only be evaluated if the blocker is NOT EBF1-associated
+    elif br == 'MYC' or br == 'B3-': # The two negatively-oriented blockers
         for loc in blockingRegions[br]:
             right_blockers_capture[loc] = cap
             right_blockers_release[loc] = rel
-    elif br == 'B3-':
-        for loc in blockingRegions[br]:
-            right_blockers_capture[loc] = cap
-            right_blockers_release[loc] = rel
-    else:
+    else: # Just for E1_1
         for loc in blockingRegions[br]:
             left_blockers_capture[loc] = cap
             left_blockers_release[loc] = rel
 
 # Manually assigning blockers in dict
+# These are the old monomer blockers.
 #left_blockers_capture[180] = TIER1_BLOCK[0] # BOUNDARY -- SHOULD NOT CHANGE **
 #left_blockers_release[180] = TIER1_BLOCK[1]
 #left_blockers_capture[210] = TIER1_BLOCK[0] # E1.2 -- BEGINNING OF PROBE 8 (BiD) -- DECREASES
@@ -104,18 +122,28 @@ right_blockers_release[495+front_buffer] = TIER4_BLOCK[1]
 #right_blockers_capture[735] = TIER1_BLOCK[0] # MYC promoter -- CENTER OF PROBE 25 -- SHOULD NOT CHANGE **
 #right_blockers_release[735] = TIER1_BLOCK[1]
 
-# Define cohesin loading regions
-E1_loading_1 = [186+front_buffer, 210+front_buffer] # Region 1
-E1_loading_2 = [221+front_buffer, 241+front_buffer] # Region 2
-E2_loading = [308+front_buffer, 318+front_buffer] # Region 3
-B3_loading_1 = [553+front_buffer, 567+front_buffer] # Region 4
-MYC_loading = [697+front_buffer, 702+front_buffer] # Region 5
-wholePolymer_loading = [0, N1-1] # Entire polymer
+########################################
+### Now we define the cohesion LOADING regions
+########################################
+E1_loading_1 = [186+front_buffer, 210+end_buffer] # Region 1
+E1_loading_2 = [221+front_buffer, 241+end_buffer] # Region 2
+E2_loading = [310+front_buffer, 318+end_buffer] # Region 3
+B3_loading_1 = [553+front_buffer, 570+end_buffer] # Region 4
+MYC_loading = [697+front_buffer, 737+end_buffer] # Region 5
+wholePolymer_loading = [0, N1-1+end_buffer] # Entire polymer
+
+### List of loading regions for EBF1 loading
 #loading_regions = [E1_loading_1, E1_loading_2, E2_loading, B3_loading_1, MYC_loading, wholePolymer_loading]
-#loading_region_freqs = [3,5,2,2,1,5]
+
+### List of loading regions for EBF1 blocking (just the entire polymer)
 loading_regions = [wholePolymer_loading]
-loading_region_freqs = [18]
+
+### Number of cohesins per loading region (each index is associated with the corresponding index of the loading_regions list)
+loading_region_freqs = [15]
 LEFNum = np.sum(loading_region_freqs)
+
+### Beyond this point, you need not change any variable values.
+###############################################################
 
 # Generate (initial) loading region spots for the loading region LEFs and list of loading site for each LEF
 LOADING_SPOTS = []
@@ -133,9 +161,6 @@ for i, region in enumerate(loading_regions):
 print(REGIONS_INDEX)
 
 EXTRUDERS = []
-
-LIFETIME = 800
-LIFETIME_STALLED = LIFETIME // 10
 targeted_idx = 0
 for i in range(len(LOADING_SPOTS)):
     #leg = np.random.randint(N)
@@ -186,6 +211,7 @@ with h5py.File(outf, mode='w') as f:
     f.attrs["LEFNum"] = LEFNum
 del EXTRUDERS
 
-#           _
-#       .__(.)< (MEOW)
-#        \___)
+
+#          _
+#      .__(.)< (MEOW)
+#       \___)
