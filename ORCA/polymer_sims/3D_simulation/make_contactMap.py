43373f9570a5129e04d5b91991533471dea2fdc0
Nuwah12 <nburget1@hotmail.com>
2024-12-07 22:22:39 -0500

    no filename option

--- ORCA/polymer_sims/3D_simulation/make_contactMap.py
+++ ORCA/polymer_sims/3D_simulation/make_contactMap.py
@@ -1,3 +1,4 @@
+#!/home/noah/.conda/envs/noah/bin/python3
 #########################
 # Script using polychrom's contact map generation implementation
 # Noah Burgt
@@ -8,12 +9,12 @@ import os
 import numpy as np
 from polychrom import contactmaps as cm
 if len(sys.argv) != 3:
-    print('Usage: python3 make_contactMaps <dir of confs> <matrix out file>')
+    print('Usage: python3 make_contactMaps <dir of confs>')
     os._exit(1)
 
 filenames = os.listdir(sys.argv[1])
 for i,x in enumerate(filenames):
     filenames[i] = "{}/{}".format(sys.argv[1],x)
 
-out = cm.monomerResolutionContactMap(filenames)
-np.savetxt(sys.argv[2], out, delimiter='\t')
+out = cm.monomerResolutionContactMap(filenames, cutoff=10)
+np.savetxt('matrix.txt', out, delimiter='\t')
