183eb3ec6b2e7dbe97e7fb1598cf55f02e783223
Nuwah12 <nburget1@hotmail.com>
2025-03-11 14:26:13 -0400

    updates

--- scripts/plotting.R
+++ scripts/plotting.R
@@ -15,24 +15,51 @@ suppressPackageStartupMessages({
 #####
 # Themes
 #####
-min.theme <- function(color="black", xtick=TRUE, )
+### Minimum theme, sets panel.background = element_blank()
+min.theme <- function(color="black", xtick=TRUE, title=NA)
 {
   if (xtick)
   {
-    return(theme(axis.ticks.y = element_line(color=color),
-                 axis.text.y = element_text(color=color),
-                 axis.ticks.x = element_line(color=color),
-                 axis.text.x = element_text(color=color),
-                 panel.background = element_blank(),
-                 axis.line = element_line(color=color)))
+    if (!is.na(title))
+    {
+      return(theme(axis.ticks.y = element_line(color=color),
+                   axis.text.y = element_text(color=color),
+                   axis.ticks.x = element_line(color=color),
+                   axis.text.x = element_text(color=color),
+                   panel.background = element_blank(),
+                   axis.line = element_line(color=color),
+                   title=element_text(title))) 
+    }
+    else
+    {
+      return(theme(axis.ticks.y = element_line(color=color),
+                   axis.text.y = element_text(color=color),
+                   axis.ticks.x = element_line(color=color),
+                   axis.text.x = element_text(color=color),
+                   panel.background = element_blank(),
+                   axis.line = element_line(color=color))) 
+    }
   }
   else
   {
-    return(theme(axis.ticks.y = element_line(color=color),
-                 axis.text.y = element_text(color=color),
-                 axis.ticks.x = element_blank(),
-                 axis.text.x = element_text(color=color),
-                 panel.background = element_blank(),
-                 axis.line = element_line(color=color)))
+    if (!is.na(title))
+    {
+      return(theme(axis.ticks.y = element_line(color=color),
+                   axis.text.y = element_text(color=color),
+                   axis.ticks.x = element_blank(),
+                   axis.text.x = element_text(color=color),
+                   panel.background = element_blank(),
+                   axis.line = element_line(color=color),
+                   title=element_text(title))) 
+    }
+    else
+    {
+      return(theme(axis.ticks.y = element_line(color=color),
+                   axis.text.y = element_text(color=color),
+                   axis.ticks.x = element_blank(),
+                   axis.text.x = element_text(color=color),
+                   panel.background = element_blank(),
+                   axis.line = element_line(color=color))) 
+    }
   }
 }
