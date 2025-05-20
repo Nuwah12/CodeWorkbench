9cecaad3d3d0edcf8f6a7e69d275c5c156f756da
Nuwah12 <nburget1@hotmail.com>
2025-05-20 16:37:17 -0400

    wip

--- imaging/FISH/piscis/piscesPipeline/piscis_pipeline.py
+++ imaging/FISH/piscis/piscesPipeline/piscis_pipeline.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
+from timeit import default_timer as timer
+from datetime import timedelta
+
 import nd2
 import tifffile
 
@@ -9,8 +12,15 @@ import piscis
 
 import argparse
 import yaml
+import logging
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__) # init the logging object
 
 def _parse_args():
+    """
+    Parse yml
+    """
     parser = argparse.ArgumentParser(prog="Piscis Pipeline",
                                      description="Call spots in smFISH data with Piscis")
     parser.add_argument("settingsFile", type=str, help="Path to settings.yml") # only one argument - the settings file
@@ -20,7 +30,7 @@ def _parse_args():
     try:
         settings = yaml.safe_load(open(args.settingsFile)) # load the yaml
     except FileNotFoundError as e:
-        print(f"{e}: Couldn't find specified settings file {args.settingsFile}. Check that it exists and re-specify.")
+        logger.error(f"{e}: Couldn't find specified settings file {args.settingsFile}. Check that it exists and re-specify.")
         exit(1)
     
     return settings
@@ -39,7 +49,7 @@ def _get_channel_dim(img, numChannels):
         if s != numChannels:
             continue
         else:
-            print(f"Inferring dim. {i} (size = {s}) is the channel dimension")
+            logger.info(f"Inferring dim. {i} (size = {s}) is the channel dimension")
             return i
 
 def _checks(args):
@@ -47,12 +57,20 @@ def _checks(args):
     if not os.path.exists(args["image_dir"]):
         raise FileNotFoundError(f"Couldn't find image directory {args['image_dir']}")
         exit(1)
+    # channel to call spots on is in list of channels
     if not args["spot_channel"] in args["channels"]:
-        raise ValueError(f"Specified channel to call spots on ({args["spot_channel"]}) is not present in provided channel names ({args["channels"]})")
+        raise ValueError(f"Specified channel to call spots on ({args["spot_channel"]}) is not present in provided channel names ({args["channels"]}). Please check the provided names and re-specify.")
         exit(1)
 
-def call_spots_piscis(piscis_obj, img):
-    pass
+def _call_spots_piscis(piscis_obj, img, threshold):
+    logger.info("Starting spot detection...")
+    start = timer()
+    pred = piscis_obj.predict(img, threshold=1)
+
+    elapsed = timedelta(seconds=timer()-start) # time
+    logger.info(f"Finished calling spots; {elapsed} elapsed")
+
+    return pred
 
 def main():
     """
@@ -67,11 +85,13 @@ def main():
     _checks(settings) # run checks on supplied argument values
     
     model = piscis.Piscis(model_name=settings["model"]) # make piscis obj to reuse
+    threshold = settings["piscis_thresh"] # piscis threshold parameter
 
     # Main loop
     for i in img_files:
+        logger.info(f"Starting image {i}")
         j = _read_img(i, imgtype) # read image to np nd array
-
+        
         channelDim = _get_channel_dim(j, len(settings["channels"])) # guesstimate the channel dimension
         channelIdx = settings["channels"].index(settings["spot_channel"]) # get the index of the spot calling channel
 
@@ -79,9 +99,8 @@ def main():
         index[channelDim] = channelIdx
         
         j = j[tuple(index)] # index the array to just the channel for spot calling
-
         
-
+        pred_spots = _call_spots_piscis(model, j, threshold)
         #print(j[tuple(index)].shape) 
         
 
