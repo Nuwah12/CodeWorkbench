aac85d285eda31c1d61059988f03807c133e557c
Nuwah12 <nburget1@hotmail.com>
2025-05-22 14:06:31 -0400

    working ver.

--- imaging/FISH/piscis/piscesPipeline/piscis_pipeline.py
+++ imaging/FISH/piscis/piscesPipeline/piscis_pipeline.py
@@ -1,22 +1,30 @@
+# standard libs
 import os
 from pathlib import Path
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-
+# plotting
+import plotly.graph_objects as go
+from PIL import Image
+import base64
+from io import BytesIO
+# timing
 from timeit import default_timer as timer
 from datetime import timedelta
-
+# image I/O
 import nd2
 import tifffile
-
+# spot calling
 import piscis
-
+# argument input / logging
 import argparse
 import yaml
 import logging
 
-logging.basicConfig(level = logging.INFO)
-logger = logging.getLogger("PiscisPipeline") # init the logging object
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger("piscis") # init the logging object
 
 def _parse_args():
     """
@@ -31,7 +39,7 @@ def _parse_args():
     try:
         settings = yaml.safe_load(open(args.settingsFile)) # load the yaml
     except FileNotFoundError as e:
-        logger.error(f"{e}: Couldn't find specified settings file {args.settingsFile}. Check that it exists and re-specify.")
+        logger.error(f" {e}: Couldn't find specified settings file {args.settingsFile}. Check that it exists and re-specify.")
         exit(1)
     
     return settings
@@ -63,7 +71,7 @@ def _get_channel_dim(img, numChannels):
         if s != numChannels:
             continue
         else:
-            logger.info(f"Inferring dim. {i} (size = {s}) is the channel dimension")
+            logger.info(f" Inferring dim. {i} (size = {s}) is the channel dimension")
             return i
 
 def _checks(args):
@@ -96,7 +104,7 @@ def _call_spots_piscis(piscis_obj, img, threshold):
     pred = piscis_obj.predict(img, threshold=1)
 
     elapsed = timedelta(seconds=timer()-start) # time
-    logger.info(f"Finished calling spots; {len(np.concatenate(pred))} spots found in {elapsed}")
+    logger.info(f" Finished calling spots; {len(np.concatenate(pred))} spots found in {elapsed}")
 
     return pred
 
@@ -108,7 +116,7 @@ def _dedup_spots(spots, img, radius):
         img (ndarray)           - image to be deduplicated
         threshold (int/float)   - diameter in pixels for finding neighborhoods
     """
-    logger.info("Deduplicating called spots")
+    logger.info(" Deduplicating called spots")
     all_spots = np.concatenate(spots) # unnest the list for all z slices
 
     # Nearest Neighbors 
@@ -137,8 +145,8 @@ def _dedup_spots(spots, img, radius):
         deduped_spots.append(all_spots[brightest_idx])
         seen.update(group)
     
-    logging.info(f"{len(deduped_spots)} spots remain after deduplication.")
-    return deduped_spots
+    logging.info(f" {len(deduped_spots)} spots remain after deduplication.")
+    return (neighborhoods, np.array(deduped_spots))
 
 # Normalize and convert Z-slice to uint8
 def _normalize_to_uint8(slice_2d):
@@ -147,7 +155,7 @@ def _normalize_to_uint8(slice_2d):
     return (norm * 255).astype(np.uint8)
 
 
-def _interactive_plot(img, spots, mode="all", outf="."):
+def _interactive_plot(img, spots, mode, outf, neighborhoods=None):
     """
     Create an interactive plot using plotly and save to html
     Arguments:
@@ -158,59 +166,142 @@ def _interactive_plot(img, spots, mode="all", outf="."):
             'dedup' - plots deduplicated spots on a max z projection of img
             'neighbors' - plots all dots on a max projection of img with lines denoting dots that were called as one neighborhood
     """
-    lines_x = []
-    lines_y = []
-    for i, nbrs in enumerate(neighbors):
-        for j in nbrs:
-            if i >= j:  # avoid duplicates
-                continue
-    lines_x.extend([x[i], x[j], None])
-    lines_y.extend([y[i], y[j], None])
-    
-    img_slice = normalize_to_uint8(loc1_untr_maxProj)
+    logger.info(f"Plotting detected spots with mode {mode}")
+    if mode == "all":
+        Z = img.shape[0]  # total number of z slices
+        frames = []
+        for z in range(Z):
+            img_slice = _normalize_to_uint8(img[z]) # get the current z slice and normalize it to [0, 255]
 
-    pil_img = Image.fromarray(img_slice)
-    buffer = BytesIO()
-    pil_img.save(buffer, format="PNG")
-    encoded = base64.b64encode(buffer.getvalue()).decode()
+            coords_z = spots[z]  # extract the list with spots from current z slice
+            y = coords_z[:, 0]   # y coord
+            x = coords_z[:, 1]   # x coord
 
-    # Create figure with image background
-    fig = go.Figure()
+            frame = go.Frame(
+                data=[
+                    go.Heatmap(  # show image
+                        z=img_slice,
+                        colorscale='gray',
+                        showscale=False),
+                    go.Scatter(  # show spots
+                        x=x,
+                        y=y,
+                        mode='markers',
+                        marker=dict(color='red', size=5),
+                        name='Spots')],
+                name=str(z))
 
-    # Add lines between neighbors
-    fig.add_trace(go.Scatter(
-        x=lines_x,
-        y=lines_y,
-        mode='lines',
-        line=dict(color='blue', width=1),
-        name='Neighbor Links'))
+            frames.append(frame)
+        
+        # Add the first frame content
+        fig = go.Figure(
+            data=[
+                go.Heatmap(z=_normalize_to_uint8(img[0]), colorscale='gray', showscale=False),
+                go.Scatter(
+                    x=spots[0][:, 1],
+                    y=spots[0][:, 0],
+                mode="markers",
+                marker=dict(color='red', size=5),
+                name='Spots')],
+            frames=frames)
 
-    # Add spot markers
-    fig.add_trace(go.Scatter(
-        x=x,
-        y=y,
-        mode='markers',
-        marker=dict(color='rgba(255,0,0,1)', size=5),
-        name='Spots'))
+        # Add slider and play buttons
+        fig.update_layout(
+            sliders=[{
+            "steps": [
+            {"method": "animate", "args": [[str(z)], {"mode": "immediate"}], "label": f"Z={z+1}"} for z in range(Z)],
+                "currentvalue": {"prefix": "Slice: "}}],
+            height=700,
+            width=700,
+            title="Z-stack Spot Viewer")
 
-    # Overlay the image
-    fig.update_layout(
-        images=[dict(
-            source=f'data:image/png;base64,{encoded}',
-            xref="x", yref="y",
-            x=0, y=0,
-            sizex=img_slice.shape[1],  # X-axis size (width)
-            sizey=img_slice.shape[0],  # Y-axis size (height)
-            sizing="stretch",
-            opacity=1.0,
-            layer="below")],
-        height=700,
-        width=700,
-        title=f"Max-Z projection with Neighbors")
+        fig.update_yaxes(autorange="reversed")  # Important for image-style orientation
+        fig.write_html(outf)
 
-    fig.update_yaxes(autorange='reversed')
-    fig.write_html('../piscis/loc01_ICOS_untr_spotNeighborhoods.html')
+    if mode == "neighbors":
+        x = spots[:,1]
+        y = spots[:,0]
+        lines_x = []
+        lines_y = []
+        for i, nbrs in enumerate(neighborhoods):
+            for j in nbrs:
+                if i >= j:  # avoid duplicates
+                    continue
+                lines_x.extend([x[i], x[j], None])
+                lines_y.extend([y[i], y[j], None])
+        
+        img_slice = _normalize_to_uint8(img)
 
+        pil_img = Image.fromarray(img_slice)
+        buffer = BytesIO()
+        pil_img.save(buffer, format="PNG")
+        encoded = base64.b64encode(buffer.getvalue()).decode()
+
+        # Create figure with image background
+        fig = go.Figure()
+
+        # Add lines between neighbors
+        fig.add_trace(go.Scatter(
+            x=lines_x,
+            y=lines_y,
+            mode='lines',
+            line=dict(color='blue', width=1),
+            name='Neighbor Links'))
+
+        # Add spot markers
+        fig.add_trace(go.Scatter(
+            x=x,
+            y=y,
+            mode='markers',
+            marker=dict(color='rgba(255,0,0,1)', size=5),
+            name='Spots'))
+
+        # Overlay the image
+        fig.update_layout(
+            images=[dict(
+                source=f'data:image/png;base64,{encoded}',
+                xref="x", yref="y",
+                x=0, y=0,
+                sizex=img_slice.shape[1],  # X-axis size (width)
+                sizey=img_slice.shape[0],  # Y-axis size (height)
+                sizing="stretch",
+                opacity=1.0,
+                layer="below")],
+            height=700,
+            width=700,
+            title=f"Max-Z projection with Neighbors")
+
+        fig.update_yaxes(autorange='reversed')
+        fig.write_html(outf)
+
+    if mode == "dedup":
+        img_slice = _normalize_to_uint8(img)
+        y = spots[:, 0]
+        x = spots[:, 1]
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Heatmap(
+            z=img,
+            colorscale='gray',
+            showscale=False
+        ))
+
+        fig.add_trace(go.Scatter(
+            x=x,
+            y=y,
+            mode='markers',
+            marker=dict(color='red', size=5),
+            name='Spots'
+        ))
+
+        fig.update_layout(
+            title="All spots",
+            height=700,
+            width=700
+        )
+        fig.update_yaxes(autorange="reversed")
+        fig.write_html(outf)
 
 def main():
     """
@@ -249,14 +340,15 @@ def main():
         pred_spots = _call_spots_piscis(model, j, threshold)
         
         # de-duplicate the spots called over all z slices
-        dedup_spots = _dedup_spots(pred_spots, j, settings["dedup_radius"])
-        
+        dedup_spots_neigh = _dedup_spots(pred_spots, j, settings["dedup_radius"])
+        dedup_spots = dedup_spots_neigh[1]
+
         if plot_all:
             _interactive_plot(j, pred_spots, mode="all", outf=f"{plot_out_dir}/{jname}_interactivePlot_allDots_allZslices.html")
         if plot_dedup:
-            _interactive_plot(_max_proj_image(j), mode="dedup", outf=f"{plot_out_dir}/{jname}_interactivePlot_dedupDots_maxProj.html")
+            _interactive_plot(_max_proj_image(j), dedup_spots, mode="dedup", outf=f"{plot_out_dir}/{jname}_interactivePlot_dedupDots_maxProj.html")
         if plot_neighborhoods:
-            _interactive_plot(_max_proj_image(j), mode="neighbors", outf=f"{plot_out_dir}/{jname}_interactivePlot_allDots_neighborhoods_maxProj.html")
+            _interactive_plot(_max_proj_image(j), np.concatenate(pred_spots), mode="neighbors", outf=f"{plot_out_dir}/{jname}_interactivePlot_allDots_neighborhoods_maxProj.html", neighborhoods=dedup_spots_neigh[0])
         exit(0)        
 
 
