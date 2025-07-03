420b742f8ac5a363cc1e100b39d6040ec5fd1c81
Nuwah12 <ngb23@pitt.edu>
2025-07-03 16:02:37 -0400

    remove jupyter nbs

--- imaging/ORCA/rename_img.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = "/mnt/data2/noah/fish_image/images_maxProj"
-
-for filename in os.listdir(folder):
-    if ".tif" in filename:
-        newname = filename.replace("_MaxProj", "")
-        #newname = newname.replace("Location", "Mask")
-        os.rename(os.path.join(folder, filename), os.path.join(folder, newname))
